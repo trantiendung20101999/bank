6714da40cedb3e81762962a2aad0df2b78975d22
trantiendung20101999 <64598849+trantiendung20101999@users.noreply.github.com>
2021-05-22 21:41:58 +0700

    update

--- app/src/main/java/com/example/bank/CreateSavingRevAdapter.java
+++ app/src/main/java/com/example/bank/CreateSavingRevAdapter.java
@@ -52,43 +52,51 @@ public class CreateSavingRevAdapter extends RecyclerView.Adapter<CreateSavingRev
             holder.btn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    SaveBankSavingBook saveBankSavingBook = new SaveBankSavingBook();
-                    saveBankSavingBook.setMoney(Double.parseDouble(CreateSavingActivity.edMoney.getText().toString()));
-                    saveBankSavingBook.setBranch("ACB BANK");
-                    UserSaveBook userSaveBook = new UserSaveBook();
-                    userSaveBook.setId(MainActivity.u.getId());
-                    long id = 0;
-                    int times = position + 1;
-                    if (type.equalsIgnoreCase("normal")) {
-                        id = position + 1;
-                    } else {
-                        id = position + 13;
-                    }
-                    InterestRateSaveBook interestRateSaveBook = new InterestRateSaveBook(id, times);
-                    saveBankSavingBook.setUser(userSaveBook);
-                    saveBankSavingBook.setInterestrate(interestRateSaveBook);
-
-                    if (saveBankSavingBook.getMoney() > MainActivity.u.getMoney()) {
+                    if (CreateSavingActivity.edMoney.getText().toString().equalsIgnoreCase("")) {
                         AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
-                        builder.setMessage("Bạn không đủ tiền ");
+                        builder.setMessage("Hãy nhập vào số tiền ");
                         builder.create().show();
-                    } else {
-                        ApiService.apiService.SaveBankSavingBook(saveBankSavingBook, "Bearer " + MainActivity.u.getAccessToken())
-                                .enqueue(new Callback<BankSavingBookResponse>() {
-                                    @Override
-                                    public void onResponse(Call<BankSavingBookResponse> call, Response<BankSavingBookResponse> response) {
-                                        Intent intent = new Intent(v.getContext(), LoginActivity.class);
-                                        intent.putExtra("alert", "Đăng nhập lại để cập nhật sổ ");
-                                        intent.putExtra("username", MainActivity.u.getUsername());
-                                        v.getContext().startActivity(intent);
-                                    }
+                    }
+                    else {
+                        SaveBankSavingBook saveBankSavingBook = new SaveBankSavingBook();
+                        saveBankSavingBook.setMoney(Double.parseDouble(CreateSavingActivity.edMoney.getText().toString()));
+                        saveBankSavingBook.setBranch("ACB BANK");
+                        UserSaveBook userSaveBook = new UserSaveBook();
+                        userSaveBook.setId(MainActivity.u.getId());
+                        long id = 0;
+                        int times = position + 1;
+                        if (type.equalsIgnoreCase("normal")) {
+                            id = position + 1;
+                        } else {
+                            id = position + 13;
+                        }
+                        InterestRateSaveBook interestRateSaveBook = new InterestRateSaveBook(id, times);
+                        saveBankSavingBook.setUser(userSaveBook);
+                        saveBankSavingBook.setInterestrate(interestRateSaveBook);
 
-                                    @Override
-                                    public void onFailure(Call<BankSavingBookResponse> call, Throwable t) {
 
-                                    }
-                                });
+                        if (saveBankSavingBook.getMoney() > MainActivity.u.getMoney()) {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                            builder.setMessage("Bạn không đủ tiền ");
+                            builder.create().show();
+                        } else {
+                            ApiService.apiService.SaveBankSavingBook(saveBankSavingBook, "Bearer " + MainActivity.u.getAccessToken())
+                                    .enqueue(new Callback<BankSavingBookResponse>() {
+                                        @Override
+                                        public void onResponse(Call<BankSavingBookResponse> call, Response<BankSavingBookResponse> response) {
+                                            Intent intent = new Intent(v.getContext(), LoginActivity.class);
+                                            intent.putExtra("alert", "Đăng nhập lại để cập nhật sổ ");
+                                            intent.putExtra("username", MainActivity.u.getUsername());
+                                            v.getContext().startActivity(intent);
+                                        }
 
+                                        @Override
+                                        public void onFailure(Call<BankSavingBookResponse> call, Throwable t) {
+
+                                        }
+                                    });
+
+                        }
                     }
                 }
             });
